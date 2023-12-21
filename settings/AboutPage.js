0b7020bd65515bdf9513518c9994e3b5811999a5
AndrewZaech <azaech021@gmail.com>
2023-12-21 08:19:28 -0500

    Settings About Page: update credits history text.

--- settings/AboutPage.js
+++ settings/AboutPage.js
@@ -214,9 +214,10 @@ class ArcMenuAboutPage extends Adw.PreferencesPage {
         creditsPage.add(historyGroup);
 
         const historyText = '<span size="small">ArcMenu was first released in 2017 by Andy C. The original ArcMenu project can be found <a href="https://gitlab.com/LinxGem33/Arc-Menu">here</a>.\n\n' +
-                            'In 2017, ArcMenu started as a fork of the Zorin menu extension by Zorin OS. ' +
+                            'In 2017, ArcMenu <i>started</i> as a fork of the Zorin menu extension by <a href="https://zorin.com/os/Zorin">Zorin OS</a>. ' +
                             "As it's own separate project, ArcMenu rapidly began developing innovative features and quickly diverged " +
-                            'away from Zorin menu thanks to the works of Andy C (2017 - 2020), Andrew Zaech (2019 - current), Alexander Rüedlinger (2017), and other contributors. ' +
+                            'away from Zorin menu thanks to the works of <a href="https://gitlab.com/LinxGem33">Andy C</a>, ' +
+                            '<a href="https://gitlab.com/AndrewZaech">Andrew Zaech</a>, <a href="https://github.com/lexruee">Alexander Rüedlinger</a>, and other contributors. ' +
                             "ArcMenu has been rewritten from the ground up since it's inception and has it's own separate, original, and unique code base, unrelated to that of Zorin menu.</span>";
 
         const historyRow = new Adw.ActionRow({
