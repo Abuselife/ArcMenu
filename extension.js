f35ce459f06999c6c71e1c30c38091c0cf580e74
AndrewZaech <azaech021@gmail.com>
2024-02-27 11:22:42 -0500

    Extension: skip any undefined panels. Add some debug logs to help #365

--- extension.js
+++ extension.js
@@ -143,22 +143,24 @@ export default class ArcMenu extends Extension {
         if (dtpActive && global.dashToPanel?.panels) {
             panels = global.dashToPanel.panels.filter(p => p);
             panelExtensionEnabled = true;
-            if (panels.length < 1)
-                console.log('ArcMenu log: Dash to Panel detected, but panels not yet created. Set panel count to 0.');
-            else
-                console.log(`ArcMenu log: Dash to Panel detected, with panel count: ${panels.length}.`);
+            console.log('ArcMenu Log: Dash to Panel enabled.');
         } else if (azTaskbarActive && global.azTaskbar?.panels) {
             panels = global.azTaskbar.panels.filter(p => p);
             panelExtensionEnabled = true;
+            console.log('ArcMenu Log: App Icons Taskbar enabled.');
         } else {
             panels = [Main.panel];
-            console.log("ArcMenu log: no panel extensions detected, set panel to 'Main.panel'.");
+            console.log('ArcMenu Log: Place ArcMenu in \'Main.Panel\'.');
         }
 
-        // If dtp or aztaskbar are active but haven't yet created their panels and ArcMenu `multi-monitor` is off,
-        // set panelsCount to the Math.min(panels.length, 1)
         const panelsCount = multiMonitor ? panels.length : Math.min(panels.length, 1);
+        console.log(`ArcMenu Log: Placing ArcMenu in ${panelsCount} panel(s)...`);
         for (var i = 0; i < panelsCount; i++) {
+            if (!panels[i]) {
+                console.log(`ArcMenu Error: panels[${i}] not found. Skipping...`);
+                continue;
+            }
+
             // Dash to Panel and AzTaskbar don't store the actual 'panel' in their global 'panels' object
             let panel = panels[i].panel ?? panels[i];
             const panelParent = panels[i].panel ? panels[i] : Main.panel;
@@ -189,6 +191,8 @@ export default class ArcMenu extends Extension {
             settingsController.enableButton();
             settingsController.connectSettingsEvents();
             this.settingsControllers.push(settingsController);
+
+            console.log(`ArcMenu Log: Placed ArcMenu in panel[${i}].`);
         }
     }
 
